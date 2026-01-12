0030d6e4e61090edb395045ebcc7cfcfa0c32ede
hamzah-pc <13378850+rezelute@users.noreply.github.com>
2026-01-12 20:11:11 +0000

    dialogLoader

---
+++ src/components/loading/DialogLoader/DialogLoader.vue
@@ -0,0 +1,30 @@
+<template>
+   <Dialog
+      :visible="visible"
+      :modal="true"
+      :dismissableMask="false"
+      :closable="false"
+      :showHeader="true"
+      :header="headerText"
+      :showFooter="false"
+      :closeOnEscape="false"
+   >
+      <div class="flex flex-col justify-center">
+         <ProgressSpinner />
+         <div v-if="loadingText" class="mt-8 text-center">{{ loadingText }}</div>
+      </div>
+   </Dialog>
+</template>
+
+<script setup lang="ts">
+import Dialog from "primevue/dialog"
+import ProgressSpinner from "primevue/progressspinner"
+
+defineProps<{
+   visible: boolean
+   headerText: string
+   loadingText?: string
+}>()
+</script>
+
+<style scoped></style>
