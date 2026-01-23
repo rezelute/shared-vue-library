890c1ee4d0d284c5304eed6fd690996bcd42ca80
hamzah-pc <13378850+rezelute@users.noreply.github.com>
2026-01-23 21:20:40 +0000

    up

--- src/components/formField/NameInput.stories.ts
+++ src/components/formField/NameInput.stories.ts
@@ -59,23 +59,23 @@ export const ShowSkeleton = {
    render: (args) => ({
       components: { NameInput },
       setup() {
-         return { args };
+         return { args }
       },
       template: `<NameInput v-bind="args" />`,
    }),
    args: {
-      label: 'Name',
-      placeholder: 'Enter your name',
+      label: "Name",
+      placeholder: "Enter your name",
       showSkeleton: true,
       isRequired: true,
       isSubmitClicked: false,
-      name: '',
+      name: "",
    },
    parameters: {
       docs: {
          description: {
-            story: 'Displays the NameInput with the skeleton loader active.',
+            story: "Displays the NameInput with the skeleton loader active.",
          },
       },
    },
-};
+}
